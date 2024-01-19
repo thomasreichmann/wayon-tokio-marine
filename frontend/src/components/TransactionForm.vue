8058afc6f028f630311a7dfcc88292d5bd71a14a
Thomas <thomasalmeidar@gmail.com>
2024-01-18 22:11:39 -0300

    estimativa da taxa de transação no formulário

--- frontend/src/components/TransactionForm.vue
+++ frontend/src/components/TransactionForm.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {defineEmits, ref} from 'vue';
+import {defineEmits, ref, watch} from 'vue';
+import {estimateFee} from "@/services/transactionService.js";
 
 const emit = defineEmits(['create']);
 
@@ -9,6 +10,17 @@ const transaction = ref({
   amount: null,
   transferDate: ''
 });
+
+const estimatedFee = ref("0");
+
+// Watch for changes in transaction.amount and transaction.transferDate
+watch(() => [transaction.value.amount, transaction.value.transferDate], () => {
+  if (transaction.value.amount && transaction.value.transferDate) {
+    estimatedFee.value = estimateFee(transaction.value);
+  } else {
+    estimatedFee.value = "0";
+  }
+}, {immediate: true});
 
 function submitForm() {
   emit('create', {...transaction.value});
@@ -20,14 +32,36 @@ function submitForm() {
   <form @submit.prevent="submitForm">
     <input v-model="transaction.originAccount" type="text" placeholder="Origin Account" required>
     <input v-model="transaction.destinationAccount" type="text" placeholder="Destination Account" required>
-    <input v-model="transaction.amount" type="number" placeholder="Amount" required>
+    <div id="estimated-fee-container">
+      <input v-model="transaction.amount" type="number" placeholder="Amount" required>
+      <span>Estimated fee: R$ {{ estimatedFee }}</span>
+    </div>
     <input v-model="transaction.transferDate" type="date" placeholder="Date" required>
     <button type="submit">Create Transaction</button>
   </form>
 </template>
 
 <style scoped>
+#estimated-fee-container {
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  width: calc(100% - 16px);
+
+  input {
+    flex-grow: 1;
+  }
+
+  span {
+    flex-shrink: 0;
+  }
+}
+
 form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   max-width: 500px;
   padding: 20px;
   border: 1px solid #333;
--- frontend/src/services/transactionService.js
+++ frontend/src/services/transactionService.js
@@ -1,3 +1,5 @@
+import {differenceInCalendarDays, parseISO} from "date-fns";
+
 const API_URL = `${import.meta.env.VITE_API_URL}/transactions`;
 
 async function fetchTransactions() {
@@ -35,4 +37,31 @@ async function createTransaction(newTransaction) {
     }
 }
 
-export {fetchTransactions, createTransaction};
+function estimateFee(transaction) {
+    if (!transaction.amount || !transaction.transferDate) {
+        return 0;
+    }
+
+    const amount = parseFloat(transaction.amount);
+    const transferDate = parseISO(transaction.transferDate);
+    const schedulingDate = new Date();
+    const daysBetween = differenceInCalendarDays(transferDate, schedulingDate);
+
+    if (daysBetween === 0) {
+        return amount * 0.025 + 3.00;
+    } else if (daysBetween >= 1 && daysBetween <= 10) {
+        return 12.00;
+    } else if (daysBetween >= 11 && daysBetween <= 20) {
+        return amount * 0.082;
+    } else if (daysBetween >= 21 && daysBetween <= 30) {
+        return amount * 0.069;
+    } else if (daysBetween >= 31 && daysBetween <= 40) {
+        return amount * 0.047;
+    } else if (daysBetween >= 41 && daysBetween <= 50) {
+        return amount * 0.017;
+    } else {
+        return 'No applicable fee';
+    }
+}
+
+export {fetchTransactions, createTransaction, estimateFee};
